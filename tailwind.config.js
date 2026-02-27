62e6dd066f40486b7132a28c1d1563f3fae41161
nasir-beep <nash144@live.com>
2026-02-27 22:32:35 +0200

    Initial commit Agritech Project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#166534", 
+        secondary: "#22c55e",
+      }
+    },
+  },
+  plugins: [],
+}
